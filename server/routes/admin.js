6f9de6c093013ac86da6dbb116e2e73904fcb9d6
Mithun <mithundamodhar@gmail.com>
2026-01-01 15:27:15 +0530

    Fix Email ETIMEDOUT: Switch to Port 587 (STARTTLS)

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -100,8 +100,8 @@ router.get('/debug-email', async (req, res) => {
         // 2. Create Transporter
         const transporter = nodemailer.createTransport({
             host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
